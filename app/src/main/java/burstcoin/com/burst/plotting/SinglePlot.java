a83338fd1ff875188e5689108330cdde6e966db3
TimTheEngineer <TimTheEngineer@Gmail.com>
2016-10-01 20:54:52 -0400

    - Allow access to Nonce # for MultiThreading in nextGen plotter

--- app/src/main/java/burstcoin/com/burst/plotting/SinglePlot.java
+++ app/src/main/java/burstcoin/com/burst/plotting/SinglePlot.java
@@ -17,13 +17,14 @@ public class SinglePlot {
     public static int SCOOP_SIZE = HASHES_PER_SCOOP * HASH_SIZE;    // 64
     public static int SCOOPS_PER_PLOT = 4096;                       // original 1MB/plot = 16384
     public static int PLOT_SIZE = SCOOPS_PER_PLOT * SCOOP_SIZE;     // 4096*64 = 262144 ; 256Kb
-
     public static int HASH_CAP = 4096;
 
     public byte[] data = new byte[PLOT_SIZE];
+    public long nonce;
 
     // Plotting Design based on URL: https://web.burst-team.us/index.php/about/
-    public SinglePlot(long addr, long nonce) {
+    public SinglePlot(long addr, long mNonce) {
+        nonce = mNonce;
         ByteBuffer base_buffer = ByteBuffer.allocate(16);
         base_buffer.putLong(addr);
         base_buffer.putLong(nonce);
@@ -48,15 +49,4 @@ public class SinglePlot {
             data[i] = (byte) (gendata[i] ^ mFinalHash[i % HASH_SIZE]);
         }
     }
-
-    /*  Don't need these
-    public byte[] getScoop(int pos) {
-        return Arrays.copyOfRange(data, pos * SCOOP_SIZE, (pos + 1) * SCOOP_SIZE);
-    }
-
-    public void hashScoop(Shabal256 md, int pos) {
-        md.update(data, pos * SCOOP_SIZE, SCOOP_SIZE);
-    }
-    */
-
 }
